63b6cce816eb86a25dfdf904017f00e9cb264751
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-03-13 14:51:42 +0200

    Extended TicTacToe class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 
 class TicTacToe {
 private:
@@ -12,7 +13,7 @@ private:
     size_t size;
 
 public:
-    TicTacToe(size_t n) {
+    explicit TicTacToe(size_t n) {
         table.reserve(n);
         size = n;
         for (int i = 0; i < n; ++i) {
@@ -41,17 +42,80 @@ public:
                 }
             }
             representation.append("\n");
-
         }
         return representation;
     }
-    int check_endgame(){
-        for(auto &row :table)
+
+    int check_endgame() {
+        std::vector<int> cross_row(size, 1);
+        std::vector<int> zero_row(size, -1);
+        for (auto &row :table) {
+            if (row == cross_row) {
+                return 1;
+            } else if (row == zero_row) {
+                return -1;
+            }
+        }
+        std::vector<int> main_diagonal;
+        std::vector<int> sub_diagonal;
+        main_diagonal.reserve(size);
+        sub_diagonal.reserve(size);
+        int j = size - 1;
+        for (int i = 0; i < size; ++i) {
+            main_diagonal.push_back(table[i][i]);
+            sub_diagonal.push_back(table[j][i]);
+            j--;
+        }
+        if (main_diagonal == cross_row || sub_diagonal == cross_row) {
+            return 1;
+        } else if (main_diagonal == zero_row || sub_diagonal == zero_row) {
+            return -1;
+        }
+        for (int i = 0; i < size; ++i) {
+            std::vector<int> column;
+            column.reserve(size);
+            for (int k = 0; k < size; ++k) {
+                column.push_back(table[k][i]);
+            }
+            if (column == cross_row) {
+                return 1;
+            } else if (column == zero_row) {
+                return -1;
+            }
+        }
+        return 0;
     }
+
+    void set_cross(int i, int j) {
+        if (i >= 0 && i < size && j >= 0 && j < size) {
+            table[i][j] = 1;
+        }
+    }
+
+    void set_zero(int i, int j) {
+        if (i >= 0 && i < size && j >= 0 && j < size) {
+            table[i][j] = -1;
+        }
+    }
+
+    std::vector<std::tuple<int, int>> get_free() {
+        std::vector<std::tuple<int, int>> free;
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if (table[i][j] == 0) {
+                    free.emplace_back(i,j);
+                }
+            }
+        }
+        return free;
+    }
+
+
 };
 
 int main() {
     auto t = new TicTacToe(5);
     std::cout << t->get_string();
+    t->check_endgame();
     return 0;
 }

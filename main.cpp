372fb56ad932193e6712b26079949341b2b0b304
Max Kmet <maxkmet@gmail.com>
2020-03-13 21:16:54 +0200

    Beta 0.1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <algorithm>
+#define X_PLAYER 1
+#define O_PLAYER -1
+#define DEFAULT_PLAYER -2
 
 class TicTacToe {
 private:
@@ -13,7 +17,7 @@ private:
     size_t size;
 
 public:
-    explicit TicTacToe(size_t n) {
+    explicit TicTacToe(size_t n = 0) {// DEFAULT ZERO NEEDED FOR CONSTRUCTOR GAME
         table.reserve(n);
         size = n;
         for (int i = 0; i < n; ++i) {
@@ -42,11 +46,12 @@ public:
                 }
             }
             representation.append("\n");
+
         }
         return representation;
     }
 
-    int check_endgame() {
+    int check_endgame() {//DRAW???
         std::vector<int> cross_row(size, 1);
         std::vector<int> zero_row(size, -1);
         for (auto &row :table) {
@@ -84,7 +89,7 @@ public:
             }
         }
         return 0;
-    }
+    };
 
     void set_cross(int i, int j) {
         if (i >= 0 && i < size && j >= 0 && j < size) {
@@ -103,19 +108,253 @@ public:
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
                 if (table[i][j] == 0) {
-                    free.emplace_back(i,j);
+                    free.emplace_back(i, j);
                 }
             }
         }
         return free;
     }
+};
 
+class AIplayer {
+private:
+    TicTacToe board;
+    int get_step_score(TicTacToe cur_board, std::tuple<int,int> coordinates, int player, int ai_player = DEFAULT_PLAYER, int current_score = 0) {
+        if (ai_player == DEFAULT_PLAYER){
+            ai_player = player;
+        }
+        if (cur_board.get_free().empty()){
+            if(cur_board.check_endgame() == ai_player)
+                return 1;
+            else
+                return 0;
+        }
+
+        if(player == X_PLAYER){
+            cur_board.set_cross(std::get<0>(coordinates), std::get<1>(coordinates));
+        }
+        else{
+            cur_board.set_zero(std::get<0>(coordinates), std::get<1>(coordinates));
+        }
+        int sum_of_subscores = 0;
+        while(!cur_board.get_free().empty()){
+            std::vector<std::tuple<int, int>>free_coords = board.get_free();
+            for(std::tuple<int,int> coords : free_coords){
+                sum_of_subscores += get_step_score(cur_board, coords, -player, ai_player, current_score);
+            }
+
+        }
+        return  sum_of_subscores;
+
+    }
+
+public:
+    AIplayer(TicTacToe &current_board) : board(board) {
+
+    }
+
+    AIplayer() = default;
+
+    std::tuple<int, int> get_next_step_naive(){
+        auto next_steps = board.get_free();
+        return next_steps[0];
+    }
+
+    std::tuple<int, int> get_next_step_tree_strategy(int player){
+        if (player != X_PLAYER && player != O_PLAYER){
+            throw std::invalid_argument("Player values should be -1 for o or 1 for x (X_PLAYER and O_PLAYER)");
+        }
+        std::vector<std::tuple<int, int>>free_coords = board.get_free();
+        if (free_coords.empty()){
+            throw std::invalid_argument("Game is over. No free coordinates");
+        }
+        std::tuple<int, int> current_best_step = free_coords[0];
+        int current_best_score = -10;
+
+        for(std::tuple<int,int> coordinates : free_coords){
+            int current_score = get_step_score(this->board, coordinates, player);//Is it copy? Should be copy
+            if (current_score > current_best_score){
+                current_best_score = current_score;
+                current_best_step = coordinates;
+            }
+        }
+        return current_best_step;
+    }
+
+};
+
+class Game{
+private:
+    AIplayer ai;
+    int AI = 0;
+    int PERSON = 1;
+
+
+    void personalized_play(){
+        std::cout << "Let's start\n";
+        std::cout << "Player 1 will play for X\n Player 2 will play for 0";
+        int num_moves = 0;
+        while (!table.get_free().empty()){//ENDGAME ???????????????????/ check is invalid for draw ???
+            if(table.check_endgame() != 0)
+                break;
+            std::tuple<int,int> next_move;
+            std::cout << table.get_string();
+            if (rival == AI && num_moves % 2 != 0){
+                next_move = ai.get_next_step_naive();
+            }
+            else {
+                next_move = get_move(num_moves % 2 + 1);
+            }
+            if(num_moves % 2 == 0){
+                table.set_cross(std::get<0>(next_move), std::get<1>(next_move));
+            } else{
+                table.set_zero(std::get<0>(next_move), std::get<1>(next_move));
+            }
+            num_moves++;
+        }
+        std::cout << table.get_string();
+        std::cout<< "GAME OVER. THE WINNER IS: \n";
+        int winner = table.check_endgame();
+        if (winner == X_PLAYER){
+            std::cout << "PLAYER 1 (X)";
+        }
+        else if (winner == O_PLAYER){
+            std::cout << "PLAYER 2 (0)";
+        }
+        else{
+            std::cout << "DRAW";
+        }
+
+
+    }
+
+    std::tuple<int,int>get_move(int player){
+        while(true) {
+            std::cout << "Player " << player << " please, enter coordinates of your step \n";
+
+            int x, y;
+            while (true) {
+                std::cout << "Please, enter x coordinate of your step (row)\n";
+                std::cin >> x;
+                x--;
+                if (std::cin.fail()) {
+                    std::cin.clear();
+                    std::cout << "Invalid input\n";
+                } else {
+                    if (x > size || x < 0) {
+                        std::cin.clear();
+                        std::cout << "Invalid input\n";
+
+                    }
+                    break;
+
+                }
+            }
+
+            while (true) {
+                std::cout << "Please, enter y coordinate of your step (column)\n";
+                std::cin >> y;
+                y--;
+                if (std::cin.fail()) {
+                    std::cin.clear();
+                    std::cout << "Invalid input\n";
+                } else {
+                    if (y > size || y < 0) {
+                        std::cin.clear();
+                        std::cout << "Invalid input\n";
+
+                    }
+                    break;
+
+                }
+            }
+
+
+            std::tuple<int, int> c{x, y};
+            std::vector<std::tuple<int, int>> free_coords = table.get_free();
+            if (std::count(free_coords.begin(), free_coords.end(), c) > 0) {
+                return c;
+            }
+            else{
+                std::cout<<"Coordinates out of range\n";
+            }
+        }
+
+    }
+
+
+    void set_rival(){
+        int option;
+        while(true) {
+            std::cout << "If you wish to play against AI enter 0 \n If you wish to play against a friend enter 1\n";
+            std::cin >> option;
+
+            if(std::cin.fail()) {
+                std::cin.clear();
+                std::cout << "Invalid input\n";
+            }
+            else {
+                if (option == 1 || option == 0){
+                    rival = option;
+                    if (rival == AI) {
+                        ai = AIplayer(table);
+                    }
+                    break;
+                }
+                std::cin.clear();
+                std::cout << "Invalid input\n";
+
+            }
+        }
+
+    }
+
+    void set_size(){
+        int option;
+        while(true) {
+            std::cout << "We offer a n*n field game \n Please, enter size of n that you wish\n";
+            std::cin >> option;
+
+            if(std::cin.fail()) {
+                std::cin.clear();
+                std::cout << "Invalid input\n";
+            }
+            else {
+                if (option > 0){
+                    size = option;
+                    table = TicTacToe(size);
+                    break;
+                }
+                std::cin.clear();
+                std::cout << "Invalid input\n";
+
+            }
+        }
+
+    }
+
+public:
+    TicTacToe table;
+    int rival;
+    int size;
+
+    Game( ) {
+    }
+
+
+    void play(){
+        std::cout << "Welcome to TIC-TAC-TOE game\n";
+        set_size();
+        set_rival();
+        personalized_play();
+
+    }
 
 };
 
 int main() {
-    auto t = new TicTacToe(5);
-    std::cout << t->get_string();
-    t->check_endgame();
+    auto g = Game();
+    g.play();
     return 0;
 }
+

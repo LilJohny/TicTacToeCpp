4e2d03606455ae658df8b312fd8be39ec23e1ac1
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-03-14 18:19:19 +0200

    Changed get_step_naive interface

--- ai_player_random.cpp
+++ ai_player_random.cpp
@@ -10,7 +10,8 @@ std::random_device   ai_player_random::m_rd;
 std::mt19937 ai_player_random::m_rng(ai_player_random::m_rd());
 
 
-std::tuple<int, int> ai_player_random::get_next_step_naive(TicTacToeBoard table) {
+std::tuple<int, int> ai_player_random::get_next_step_naive(std::tuple<int, TicTacToeBoard> state) {
+    auto table = std::get<1>(state);
     auto next_steps = table.get_free();
     std::uniform_real_distribution<double> dist(0, next_steps.size());
     int ind = dist(m_rng);
